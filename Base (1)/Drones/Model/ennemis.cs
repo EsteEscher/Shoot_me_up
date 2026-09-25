144d15f1ffbb121e5f56dcc796c1896b374f033b
EsteEscher <escher.este@gmail.com>
2026-09-25 16:20:26 +0200

    Feat(code) les ennemis poursuivent le joueur maintenant quand ils sont proches
    [30,Done]

--- Base (1)/Drones/Model/ennemis.cs
+++ Base (1)/Drones/Model/ennemis.cs
@@ -3,8 +3,10 @@ using Player.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.AxHost;
 
 namespace Player
 {
@@ -14,32 +16,57 @@ namespace Player
         public int hp;                                // Ces Pv                      
         private int _x;                                 // Position en X depuis la gauche de l'espace aérien
         private int _y;                                 // Position en Y depuis le haut de l'espace aérien
-        private Random _alea = new Random();
         public const int HEIGHTSIZE = 62;
         public const int WIDTHSIZE = 50;
         private int _targetX;
+        public State state = State.TRAVELLING;
+        private Player _player;
+
+        public enum State { TRAVELLING, SEEKING, STOPPING, EXPLOSING }
+
 
         public int Y { get => _y; set => _y = value; }
         public int X { get => _x; set => _x = value; }
 
         // Constructeur
-        public Ennemis(int x, int y)
+        public Ennemis(int x, int y, Player player)
         {
             Random alea = new Random();
             this.X = x;
             this.Y = y;
             hp = 3; // Leur Pv
+            this._player = player;
         }
 
         // Cette méthode calcule le nouvel état dans lequel le drone se trouve après
         // que 'interval' millisecondes se sont écoulées
         public void Update(int interval)
         {
-            if (_y != GameSpace.HEIGHT)
+            if (_y <= GameSpace.HEIGHT - 200)
             {
-                _y++;
+                state = State.TRAVELLING;
+                _y += 5;
+            
             }
-                
+            if (_y >= GameSpace.HEIGHT - 200)
+            {
+                if (_player.x < _x)
+                {
+                    _x--;
+                }
+                if (_player.y < _y)
+                {
+                    _y--;
+                }
+                if (_player.x > _x)
+                {
+                    _x++;
+                }
+                if (_player.y > _y)
+                {
+                    _y++;
+                }
+            }
         }
 
         // Choisit une nouvelle vitesse aléatoirement
